8dab384a92044aaec1067174dc75a818b4365596
Scotty Fulton <sfulton@talentpath.com>
2019-12-07 08:56:55 -0600

    working on the upload of imageprocessed

--- routes/image.js
+++ routes/image.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Image = require("../models/Images.js");
+const Jimp = require("jimp");
+const fs = require("fs");
+const FileBase = require("react-file-base64");
 
 // get one
 router.get("/findOne", async (req, res) => {
@@ -33,24 +36,60 @@ router.route("/uploadbase").post((req, res, next) => {
         companyName: req.body.companyName,
         imageBase64: req.body.imageBase64
     });
-    newImage
-        .save()
-        .then(result => {
-            res.status(200).json({
-                success: true,
-                document: result
-            });
-            console.log("it it now");
-        })
-        .catch(err => next(err));
+    console.log(
+        "newImage.imageBase64: ",
+        req.body.imageBase64.toString("base64")
+    );
+    // pImg(newImage.imageBase64.toString("base64"));
+    var bitmap = base64_encode("_smallerImg.png");
+    fs.writeFile("_smallerImg.png", bitmap, err => {
+        if (err) throw err;
+    });
+    // Jimp.read("_smallerImg.png", (err, jupiter) => {
+    //     if (err) throw err;
+    //     jupiter
+    //         .resize(256, 256) // resize
+    //         .quality(60) // set JPEG quality
+    //         // .greyscale() // set greyscale
+    //         // .invert()
+    //         .write("smallerImg.png"); // save
+    // });
+
+    // var fs = require("fs");
+
+    function base64_encode(file) {
+        // read binary data
+        var bitmap = fs.readFileSync(file);
+        // convert binary data to base64 encoded string
+        return new Buffer(bitmap).toString("base64");
+    }
+
+    // var bitmap = base64_encode("_smallerImg.png");
+    // newImage
+    //     .save()
+    //     .then(result => {
+    //         res.status(200).json({
+    //             success: true,
+    //             document: result
+    //         });
+    //         console.log("it it now");
+    //     })
+    //     .catch(err => next(err));
 });
-// .get(function(req, res) {
-//     Image.findOne({}, "img createdAt", function(err, img) {
-//         if (err) res.send(err);
-//         // console.log(img);
-//         res.contentType("json");
-//         res.send(img);
-//     }).sort({ createdAt: "desc" });
-// });
+
+function pImg(file) {
+    // const pImg = () =>
+    // argv[2] is first cmdline argument passed
+    // Jimp.read(process.argv[2], (err, jupiter) => {
+    Jimp.read(file, (err, jupiter) => {
+        if (err) throw err;
+        jupiter
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            // .greyscale() // set greyscale
+            // .invert()
+            .write("smallerImg.png"); // save
+    });
+}
 
 module.exports = router;
