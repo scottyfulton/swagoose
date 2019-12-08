573ae4d5ae56e7b13d80e7e3fc8c7377099444e8
Scotty <scotty.fulton01@gmail.com>
2019-12-07 21:22:41 -0600

    maybe with app.js change

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3003;
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("routes"));
 }
 
 //import routes
