918d184f1af45006ad82f7bec4f0fdf2f9b23181
Scotty <scotty.fulton01@gmail.com>
2019-12-08 23:04:43 -0600

    CORS damn it

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ const imageRoute = require("./routes/image");
 app.use(cors());
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    );
+    next();
+});
 
 if (process.env.NODE_ENV === "production") {
     // app.use(express.static("__dirname"));
